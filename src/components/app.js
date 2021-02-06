82921969e4f9cb75c85a51c0da98a34896858939
sahalgithub <muse.salah@gmail.com>
2021-02-05 19:24:05 -0600

    setup logo component

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Logo from './logo';
 
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>logo</h2>
+        <Logo />
       </div>
     );
   }
