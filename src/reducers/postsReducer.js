e12964cb37259ab1af9626b6fb01ab801573f401
sahalgithub <muse.salah@gmail.com>
2021-02-10 12:20:06 -0600

    added SET_RESULTS_POSTS reducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,10 +1,12 @@
 import {
-    SET_RECENT_POSTS
+    SET_RECENT_POSTS,
+    SER_RESULTS_POSTS
 } from '../actions/types';
 
 const INIT_STATE = {
     posts: [],
-    recentPosts: []
+    recentPosts: [],
+    resultsPosts: []
 }
 
 export default function(state = INIT_STATE, action) {
@@ -15,6 +17,13 @@ export default function(state = INIT_STATE, action) {
                     ...state,
                     recentPosts
                 }
+
+        case SET_RESULTS_POSTS:
+            const resultsPosts = action.payload;
+            return {
+                ...state,
+                resultsPosts
+            }
         default:
             return state;
     }
