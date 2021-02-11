75c2805e486400ec7d2b08d11521a7231c0af74c
sahalgithub <muse.salah@gmail.com>
2021-02-11 17:42:19 -0600

    sytled results component searchbar

--- src/components/results.js
+++ src/components/results.js
@@ -15,7 +15,7 @@ class Results extends Component {
         return (
             <div classNames="results">
                 <Logo size={55} />
-                <SearchBar onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
+                <SearchBar page="results" onSubmit={(query) => this.handleSearchBarSubmit(query)}/>
                 <ResultsPosts />
             </div>
         )
