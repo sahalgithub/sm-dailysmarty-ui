526d9f79d144a75c8e5d19f4b47c97a9599faf78
sahalgithub <muse.salah@gmail.com>
2021-02-12 11:28:26 -0600

    finished styling the result post component

--- src/components/post.js
+++ src/components/post.js
@@ -47,12 +47,13 @@ class Post extends Component {
             )
         } else if(this.props.type == 'result') {
             return (
-                <li className="results-post">
+                <li className="results-post"
+                    onMouseEnter={() => this.setState({ height: 70})}
+                    onMouseLeave={() => this.setState({ height: 0})}
+                
+                >
                     <div className="result-post__title">
-                        <a href={this.props.url_for_post}
-                            onMouseEnter={() => this.setState({ height: 70})}
-                            onMouseLeave={() => this.setState({ height: 0})}
-                        >
+                        <a href={this.props.url_for_post}>
                             {this.props.title}</a>
                     </div>  
                     <div className="results-post__title">
